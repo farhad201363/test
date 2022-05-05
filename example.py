8cfc95d02515a30c3a3918d775dfd2e72e3e199a
Farhad <fsaffaraval@gmail.com>
2022-05-05 21:43:12 +0000

    adding a file.

---
+++ example.py
@@ -0,0 +1,6 @@
+  GNU nano 2.7.4                                                                                                 File: example.py                                                                                                  Modified
+
+def git_opeation():
+        printdef git_opeation():
+        print
+
